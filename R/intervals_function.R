cf5d3df5c2e0048ab8dd476982e0e728cb849d0a
Gnbzler <rethilo@gmail.com>
2025-01-07 10:50:37 +0100

    V0.2 Added a function for calculating intervals

--- R/intervals_function.R
+++ R/intervals_function.R
@@ -1,3 +1,21 @@
+#' Calculating Intervals
+#'
+#' This function calculates intervals for a given break-duration between a given start time (start) and end time (end) for multiple recordings. Returns only the intervals as a vector to append to existing dataframe.
+#' @param timetable dataframe containing minimum two columns indicating starting point and end point of each recording (each row is one recording); columns need to be called start and end for the designated timestamps
+#' @param breaks Duration between the intervals given as a String. Defaults to 1 min
+#' @keywords bats HK intervals
+#' @export
+cal_intervals <- function(timetable, breaks = "1 min"){
+  temp <- timetable %>%
+    rowwise() %>%
+    mutate(interval_list = list(seq(start, end, by = "1 min"))) %>%  # Zeitintervalle erstellen
+    unnest(interval_list) %>%
+    rename(interval_start = interval_list)
+  return(temp$interval_start)
+}
+
+
+
 #' Counting contacts for given Species
 #'
 #' This function counts the number of contacts across set intervals provided filtering beforehand by a single species.
@@ -8,8 +26,6 @@
 #' @param interval_length Length of the interval in seconds. Defaults to 60s
 #' @keywords bats HK
 #' @export
-#' @examples
-#' cat_function()
 count_contacts_per_interval_of_sp <- function(intervals, rufe_HKX, art, interval_length = 60) {
   # Filter call dataset based on the given species (Art)
   temp_ruf <- rufe_HKX %>%
@@ -26,3 +42,4 @@ count_contacts_per_interval_of_sp <- function(intervals, rufe_HKX, art, interval
   # return the results as a vector
   return(temp$contact_count)
 }
+
